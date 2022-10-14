70f56fb56cf42dd4fd808ca6be4620afeb5de1ad
Ostandale <dio7890@gmail.com>
2022-10-14 10:07:36 +0900

    test2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,7 +13,7 @@ use std::env;
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-
+    //
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![tauri_commands::test1_func,])
         .run(tauri::generate_context!())
